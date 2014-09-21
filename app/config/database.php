b85e9a9c169418b7c7c376b823b7926d6014c578
Vrigz Alejo <nuedu2012@gmail.com>
2014-09-21 20:02:01 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'MAMCORTEZ_QUIZES',
 			'username'  => 'root',
-			'password'  => 'vrigz15mae',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
